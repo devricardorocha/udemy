77ea7b7961f81c5f3c589e9f3af7357a44f1dc81
Ricardo Rocha <dev.ricardorocha@gmail.com>
2026-07-15 08:53:48 -0300

    new version and db

--- aws_project/src/main/java/br/com/udemy/aws_ecs_fargate_java/aws_project/controller/TestController.java
+++ aws_project/src/main/java/br/com/udemy/aws_ecs_fargate_java/aws_project/controller/TestController.java
@@ -2,10 +2,10 @@ package br.com.udemy.aws_ecs_fargate_java.aws_project.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.Random;
 
 @Log4j2
 @RestController
@@ -14,8 +14,17 @@ public class TestController {
 
     @GetMapping("/dog/{name}")
     public ResponseEntity<?> dogTest(@PathVariable String name) {
-        log.info("Test controller - name: {}", name);
+        log.info("Test controller - dog name: {}", name);
         return ResponseEntity.ok("Name: " + name);
+    }
+
+    @GetMapping("/random-number")
+    public ResponseEntity<?> getRandomNumber(@RequestParam(required = false) Integer begin, @RequestParam(required = false) Integer end) {
+        final Integer realBegin = Optional.ofNullable(begin).orElse(0);
+        final Integer realEnd = Optional.ofNullable(end).orElse(Integer.MAX_VALUE);
+        final Integer result = new Random().ints(realBegin, realEnd).limit(1).findFirst().orElse(-1);
+        log.info("Test controller - random number: {}", result);
+        return ResponseEntity.ok("Number: " + result);
     }
 
 }
--- aws_project_cdk/src/main/java/com/myorg/AwsProjectCdkApp.java
+++ aws_project_cdk/src/main/java/com/myorg/AwsProjectCdkApp.java
@@ -15,8 +15,12 @@ public class AwsProjectCdkApp {
         ClusterStack clusterStack = new ClusterStack(app, "Cluster", vpcStack.getVpc());
         clusterStack.addDependency(vpcStack);
 
+        RDSStack rdsStack = new RDSStack(app, "Rds", vpcStack.getVpc());
+        rdsStack.addDependency(vpcStack);
+
         Service01Stack service01Stack = new Service01Stack(app, "Service01", clusterStack.getCluster());
         service01Stack.addDependency(clusterStack);
+        service01Stack.addDependency(rdsStack);
 
         app.synth();
     }
